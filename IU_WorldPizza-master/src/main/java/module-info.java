b547a6c0427fe8dc3b4fb3bc5cdd5158ebc78d59
Bryan Emmanuel Cetzal Ceme <91039569+BryanCetzal@users.noreply.github.com>
2022-12-09 08:34:40 -0600

    Delete IU_WorldPizza-master directory

--- IU_WorldPizza-master/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.app.interfazusuario_wp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.app.interfazusuario_wp to javafx.fxml;
-    exports com.app.interfazusuario_wp;
-}
